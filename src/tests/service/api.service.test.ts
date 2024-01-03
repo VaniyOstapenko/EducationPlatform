4a59f3abedd0c06331b651932a89401ff9e9bcf8
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2024-01-03 23:46:42 +0300

    EducationPlatform

--- src/tests/service/api.service.test.ts
+++ src/tests/service/api.service.test.ts
@@ -1,6 +1,7 @@
-import { registrationUser} from '../../service/api.service';
+import { registrationUser, authUser } from '../../service/api.service';
 import * as repository from '../../repository/api.repository';
 import ExceptionType from '../../helper/exception';
+import bcrypt from 'bcrypt';
 
 describe('test registrationUser function', () => {
   test('succees', async () => {
@@ -14,13 +15,61 @@ describe('test registrationUser function', () => {
     expect(result).toEqual([{ id: 1, name: 'Ivan', surname: 'Ostapenko', email: 'Osta@mail.com', pwd: 'Vaniy245' }]);
   });
 
+  test('error getUserByEmail from registrationUser', async () => {
+    const repFunction = jest.spyOn(repository, 'getUserByEmailDB');
+    repFunction.mockResolvedValue([{ id: 1, name: 'Ivan', surname: 'Ostapenko', email: 'Osta@mail.com', pwd: 'Vaniy245' }]);
+    try {
+      await registrationUser('Ivan', 'Ostapenko', 'Osta@mail.com', 'Vaniy245');
+    } catch (error: any) {
+      expect(error.message).toBe(ExceptionType.DATA_API_FOUND_USER);
+    }
+  });
+
   test('error', async () => {
+    const repFunction2 = jest.spyOn(repository, 'getUserByEmailDB');
+    repFunction2.mockResolvedValue([]);
     const repFunction = jest.spyOn(repository, 'registrationUserDB');
     repFunction.mockResolvedValue([]);
     try {
       await registrationUser('Ivan', 'Ostapenko', 'Osta@mail.com', 'Vaniy245');
     } catch (error: any) {
       expect(error.message).toBe(ExceptionType.DATA_API_LENGTH);
+    }
+  });
+});
+
+describe('test authUser function', () => {
+  test('success', async () => {
+    const repFunction2 = jest.spyOn(repository, 'getUserByEmailDB');
+    repFunction2.mockResolvedValue([{ id: 1, name: 'Ivan', surname: 'Ostapenko', email: 'Osta@mail.com', pwd: 'Vaniy245' }]);
+    const repFunction = jest.spyOn(bcrypt, 'compare');
+    repFunction.mockResolvedValue(true);
+    const result = await authUser('Ostapeko@mail.com', 'Vaineg2352');
+    expect(repFunction).toHaveBeenCalled();
+    expect(result[0].id).toBe(1);
+    expect(result.length).toBe(1);
+    expect(result).toEqual([{ id: 1, name: 'Ivan', surname: 'Ostapenko', email: 'Osta@mail.com', pwd: 'Vaniy245' }]);
+  });
+
+  test('error getUserByEmail from authUser', async () => {
+    const repFunction = jest.spyOn(repository, 'getUserByEmailDB');
+    repFunction.mockResolvedValue([]);
+    try {
+      await authUser('Osta@mail.com', 'Vaniy245');
+    } catch (error: any) {
+      expect(error.message).toBe(ExceptionType.DATA_AUTH_USER);
+    }
+  });
+
+  test('error pwd fron authUser function', async () => {
+    const repFunction2 = jest.spyOn(repository, 'getUserByEmailDB');
+    repFunction2.mockResolvedValue([{ id: 1, name: 'Ivan', surname: 'Ostapenko', email: 'Osta@mail.com', pwd: 'Vaniy245' }]);
+    const repFunction = jest.spyOn(bcrypt, 'compare');
+    repFunction.mockResolvedValue(false);
+    try {
+      await authUser('Osta@mail.com', 'Vaniy245');
+    } catch (error: any) {
+      expect(error.message).toBe(ExceptionType.DATA_AUTH_PWD);
     }
   });
 });
