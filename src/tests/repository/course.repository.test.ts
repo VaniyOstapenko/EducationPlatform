7507c904e38c51aa31ae64c319ca701f03167c11
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2024-01-06 17:38:32 +0300

    EducationPlatform

--- src/tests/repository/course.repository.test.ts
+++ src/tests/repository/course.repository.test.ts
@@ -1,4 +1,4 @@
-import { getAllCourseDB, getCourseByIdDB, createCourseDB } from '../../repository/course.repository';
+import { getAllCourseDB, getCourseByIdDB, createCourseDB, deleteCourseDB, updateCourseDB } from '../../repository/course.repository';
 
 const mClient = {
   query: jest.fn(),
@@ -40,6 +40,28 @@ describe('test createCourseDB function', () => {
   test('should return success', async () => {
     mClient.query.mockResolvedValue({ rows: [{ id: 1, course: 'TS' }] });
     const result = await createCourseDB('TS');
+    expect(mClient.query).toHaveBeenCalled();
+    expect(result[0].id).toBe(1);
+    expect(result).toHaveLength(1);
+    expect(result).toEqual([{ id: 1, course: 'TS' }]);
+  });
+});
+
+describe('test updateCourseDB', () => {
+  test('should return success', async () => {
+    mClient.query.mockResolvedValue({ rows: [{ id: 1, course: 'TS' }] });
+    const result = await updateCourseDB(1, 'TS');
+    expect(mClient.query).toHaveBeenCalled();
+    expect(result[0].id).toBe(1);
+    expect(result).toHaveLength(1);
+    expect(result).toEqual([{ id: 1, course: 'TS' }]);
+  });
+});
+
+describe('test deleteCourseDB', () => {
+  test('should return success', async () => {
+    mClient.query.mockResolvedValue({ rows: [{ id: 1, course: 'TS' }] });
+    const result = await deleteCourseDB(1);
     expect(mClient.query).toHaveBeenCalled();
     expect(result[0].id).toBe(1);
     expect(result).toHaveLength(1);
