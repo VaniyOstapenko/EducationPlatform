b1dc2f7c26937699370d98eeb14f760f382175f6
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2024-01-18 23:33:06 +0300

    EducationalPlatform

---
+++ Client/src/components/Preview/Preview.js
@@ -0,0 +1,36 @@
+import style from './style.module.css'
+
+function Preview() {
+    return (
+        <div className={style.wrapper}>
+            <div className={style.previewContent}>
+                <div className={style.previewInfo}>
+                    <p className={style.textPlatform}>E-COURSE PLATFORM</p>
+                    <h1>Learning and
+                        teaching online,
+                        made easy.</h1>
+                    <p className={style.textAdditional}>Any subject, in any language, on any device, for all ages!</p>
+                    <div className={style.btn}>About platform</div>
+                    <div className={style.statistics}>
+                        <div className={style.lightningImg}></div>
+                        <p className={style.studentsCount}>600<span>+</span></p>
+                    </div>
+                    <p className={style.textStudents}>Students</p>
+                </div>
+                <div className={style.previewImg}></div>
+            </div>
+
+            <div className={style.learnContent}>
+
+            </div>
+            <div className={style.knowledgeContent}>
+
+            </div>
+            <div className={style.progressContent}>
+
+            </div>
+        </div>
+    )
+}
+
+export default Preview;
